c75beb237a5f8a62f45cd94fc70500e8ecce8282
EbenauDev <ebenau06@gmail.com>
2024-08-17 17:53:09 -0300

    Criado alguns scripts iniciais da estrutura sql

---
+++ api/SistemaDeAgendamentos/SistemaDeAgendamentos.API/Infra/Scripts/agendamentos.sql
@@ -0,0 +1,33 @@
+﻿create database Agendamentos
+
+use Agendamentos;
+
+CREATE TABLE Pessoa (
+	Id INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+	Nome VARCHAR(50) NOT NULL,
+	Email VARCHAR(50) NOT NULL,
+	Celular VARCHAR(20) NULL,
+	CPF VARCHAR(11)
+)
+
+
+CREATE TABLE Espaco (
+	Id INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+	Nome VARCHAR(50) not null,
+	Descricao VARCHAR(300)
+)
+
+CREATE TABLE EspacoConfiguracaoHorarios (
+	Id INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+	IdEspaco Int NOT NULL,
+	DiaDaSemana INT,
+	HorarioInicial CHAR(5),
+	HorarioFinal CHAR(5),
+	Duracao CHAR(5),
+	IntervaloEntreHorarios CHAR(5)
+)
+GO
+
+ALTER TABLE EspacoConfiguracaoHorarios
+ADD CONSTRAINT FK_IdEspaco FOREIGN KEY (IdEspaco)
+REFERENCES Espaco (Id)
